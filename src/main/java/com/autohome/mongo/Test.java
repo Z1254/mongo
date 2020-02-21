c56f5f4fbb9267200bae2a7d8cff04509d578b02
zzn <122925636@qq.com>
2020-02-21 14:34:23 +0800

    测试提交

---
+++ src/main/java/com/autohome/mongo/Test.java
@@ -0,0 +1,11 @@
+package com.autohome.mongo;
+
+/**
+ * @author zzn
+ * @date 2020/2/21 14:33
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
