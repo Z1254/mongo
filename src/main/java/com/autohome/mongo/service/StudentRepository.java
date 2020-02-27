2dc1b4f7c3cdfbe1e48a297748d3e234b65077be
zzn <122925636@qq.com>
2020-02-27 15:10:34 +0800

    111

--- src/main/java/com/autohome/mongo/service/StudentRepository.java
+++ src/main/java/com/autohome/mongo/service/StudentRepository.java
@@ -1,11 +1,11 @@
 package com.autohome.mongo.service;
 
-import com.autohome.mongo.pojo.Student;
+import com.autohome.mongo.pojo.Entity_DataCount;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 /**
  * @author zzn
  * @date 2020/2/23 19:48
  */
-public interface StudentRepository extends MongoRepository<Student, String> {
+public interface StudentRepository extends MongoRepository<Entity_DataCount, String> {
 }
